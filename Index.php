4f8a2164bb0cf508b960c92fa55ecc3bab80e600
José Haroldo Donis Alonso <66327201+201801444@users.noreply.github.com>
2020-11-20 21:01:35 -0600

    Agregamos fondo azul a la pagina web

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BBDD MYSQL PHP</title>
+    <title>MVC CRUD PHP</title>
     <style>
         h1{text-align: center;font-size: 50px ; margin: 60px}
     </style>
